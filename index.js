f2b8c573d4e19208d186bf662f39306a7c295ffa
Lidor Israeli <65282019+Lidorisr@users.noreply.github.com>
2022-06-13 20:13:56 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,95 @@
-const chars = [
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+~\\`|}{[]:;?><,./-=0123456789",
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
 ];
 
 const passFieldsEl1 = document.getElementById("pass-field1");
@@ -7,16 +97,14 @@ const passFieldsEl2 = document.getElementById("pass-field2");
 const passFieldsEl3 = document.getElementById("pass-field3");
 const passFieldsEl4 = document.getElementById("pass-field4");
 
-function password() {
+const password = () => {
   let pass = "";
-  let str = chars[0];
-
   for (let i = 0; i <= 15; i++) {
-    const char = Math.floor(Math.random() * str.length + 1);
-    pass += str.charAt(char);
+    const char = characters[Math.floor(Math.random() * characters.length)];
+    pass += char;
   }
   return pass;
-}
+};
 
 function generatePass() {
   passFieldsEl1.value = password();
